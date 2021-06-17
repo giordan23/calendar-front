9ee443a0148441f7edd9cafc69dac1b6f77ba015
Giordan <giordan.23w@gmail.com>
2021-06-17 12:47:14 -0500

    desde Fer Herrera

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    checking: true,
+    // uid: null,
+    // name: null
+}
+
+export const authReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+
+
+    default:
+        return state
+    }
+}
---
+++ src/types/types.js
@@ -0,0 +1,22 @@
+
+export const types = {
+
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+
+    eventSetActive: '[event] Set Active',
+    eventAddNew: '[event] Add new',
+    eventClearActiveEvent: '[event] Clear active event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+
+    authChecking : '[auth] Checking login state',
+    authCheckingFinish : '[auth] Finish checking login state',
+    authStartLogin : '[auth] Start Login',
+    authLogin : '[auth] Login',
+    authStartRegister : '[auth] Checking login state',
+    authStartTokenRenew : '[auth] Start token renew',
+    authLogout : '[auth] Logout'
+
+}
